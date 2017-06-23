acf43ee0c4f40a8d3e02856a6056e1ced252c573
Jeff Schornick <jeff@schornick.org>
2017-06-23 01:37:04 -0600

    [P1] Intitial version of Make-based build system

---
+++ project1/src/sources.mk
@@ -0,0 +1,24 @@
+# sources.mk
+#
+# Makefile include that explicitly defines our source files
+
+# Common source files, all platforms
+SOURCES = \
+  conversion.c \
+  debug.c \
+  main.c \
+  memory.c \
+  project1.c
+
+# Platform-specific source files
+ifeq ($(PLATFORM),HOST)
+  # no additional sources
+else ifeq ($(PLATFORM),BBB)
+  # no additional sources
+else ifeq ($(PLATFORM),KL25Z)
+  SOURCES += system_MKL25Z4.c
+  ASM_SOURCES += startup_MKL25Z4.S
+else
+  $(error Invalid PLATFORM specified, must be one of: HOST, BBB, KL25Z)
+endif
+
